8f3b5397dd0811ff7c9d41867483a6ea1f5506d7
zjs <282581063@qq.com>
2018-10-13 19:54:03 +0800

    深度延迟加载

--- src/main/java/com/zzu/ssm/mapper/UserMapper.java
+++ src/main/java/com/zzu/ssm/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.zzu.ssm.mapper;
 
 import java.util.List;
 
+import com.zzu.ssm.po.OrdersExt;
 import com.zzu.ssm.po.User;
 
 public interface UserMapper {
@@ -22,4 +23,19 @@ public interface UserMapper {
 	 * @return
 	 */
 	public List<User> findUserListResultMap(); 
+	/**
+	 * 测试resultMap映射
+	 * 一对一关联查询时，可以定义专门的扩展po类作为输出结果类型
+	 * @return
+	 */
+	public List<OrdersExt> findOrdersList();
+	
+	
+	public List<OrdersExt> findOrdersAndUserRstMap() throws Exception;
+	/**
+	 * 测试resultMap映射
+	 * 一对多关联查询时
+	 * @return
+	 */
+	public List<User> findUserAndOrderRstMap(int id) throws Exception; 
 }
